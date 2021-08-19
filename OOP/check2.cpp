057228f617b64634630a1d1dee4ccc7c9a82bbc5
Soumalya1857 <soumalyakundu10@gmail.com>
2021-08-19 21:30:20 +0530

    Dump added

--- OOP/check2.cpp
+++ OOP/check2.cpp
@@ -19,6 +19,8 @@ class base
         {
             return a+b+10;
         }
+
+        virtual void print(){cout << "this is base class\n";}
 };
 
 class derived: public base
@@ -31,6 +33,8 @@ class derived: public base
         // }
 
         void show(){cout << "blablabla\n";}
+
+        void print(){cout << "This is derived class\n";}
 };
 
 
@@ -39,10 +43,13 @@ int main()
     base b;
     derived d;
 
-    cout << "b.add(5,3):" << " ";
-    cout << b.add(5,3) << endl;
+    // cout << "b.add(5,3):" << " ";
+    // cout << b.add(5,3) << endl;
 
-    cout << "d.add(5.3,3.2): " << " " << d.add(5.3f,3.2f) << endl;
+    // cout << "d.add(5.3,3.2): " << " " << d.add(5.3f,3.2f) << endl;
+    base* bptr = &d;
+    bptr->print();
+    d.print();
 
     return 0;
 }
